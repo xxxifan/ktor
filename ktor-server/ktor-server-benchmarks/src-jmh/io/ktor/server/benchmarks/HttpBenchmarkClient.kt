dbf6e5aee71437c37b024213e1ee19510d7428e2
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-01-25 12:48:35 +0300

    Add async benchmark runner

--- ktor-server/ktor-server-benchmarks/src-jmh/io/ktor/server/benchmarks/HttpBenchmarkClient.kt
+++ ktor-server/ktor-server-benchmarks/src-jmh/io/ktor/server/benchmarks/HttpBenchmarkClient.kt
@@ -1,15 +1,9 @@
 package io.ktor.server.benchmarks
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.request.*
-import io.ktor.client.response.*
-import kotlinx.coroutines.experimental.*
 import okhttp3.*
 import org.apache.http.client.methods.*
 import org.apache.http.impl.client.*
 import java.net.*
-import java.nio.*
 
 interface HttpBenchmarkClient {
     fun setup()
@@ -73,29 +67,5 @@ class OkHttpBenchmarkClient : HttpBenchmarkClient {
             while (stream.read(buf) != -1);
         }
         response.close()
-    }
-}
-
-class KtorBenchmarkClient : HttpBenchmarkClient {
-    var httpClient: HttpClient? = null
-
-    override fun setup() {
-        httpClient = HttpClient(CIO)
-    }
-
-    override fun shutdown() {
-        httpClient?.close()
-        httpClient = null
-    }
-
-    override fun load(url: String) = runBlocking {
-        val buffer = ByteBuffer.allocate(1024)
-        httpClient!!.get<HttpResponse>(url).use { response ->
-            val channel = response.receiveContent().readChannel()
-            while (!channel.isClosedForRead) {
-                buffer.clear()
-                channel.readAvailable(buffer)
-            }
-        }
     }
 }
